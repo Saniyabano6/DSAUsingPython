dba3611e46b1dad2f6eaca735e9be06042e26d2d
Saniya <shaikhsaniyabano6@gmail.com>
2026-07-20 13:56:50 +0530

    Time: 54 ms (65.08%) | Memory: 19.5 MB (13.41%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        minprice=float("inf")
+        maxprofit=0
+        for i in range(len(prices)):
+            if prices[i]<minprice:
+                minprice=prices[i]
+            elif prices[i]-minprice>maxprofit:
+                maxprofit=prices[i]-minprice
+        return maxprofit     
